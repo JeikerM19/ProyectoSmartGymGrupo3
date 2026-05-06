56ae11b3880f41150dce5a00cb835e98329540b8
José Alaña <joserobertoalana60@gmail.com>
2026-05-06 09:22:34 -0400

    configuracion del token

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     class Config:
         env_file = ".env"
